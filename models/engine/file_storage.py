61357c27d1f68fadf79f36d8d6ff800a9d8e6409
T <test@mail.com>
2019-09-09 20:00:50 -0700

    add two new methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """method to retrieve one object"""
+        key = "{}.{}".format(cls, id)
+        if key in self.__objects:
+            return self.__objects[key]
+        return None
+
+    def count(self, cls=None):
+        """method to count the number of objects"""
+        if not cls:
+            return len(self.__objects)
+        return len(self.all(cls))
