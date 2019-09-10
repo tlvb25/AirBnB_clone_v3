d2ceb8678dded45afb9285ad9220938b1c32da64
T <test@mail.com>
2019-09-09 21:11:35 -0700

    corr

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,8 +79,8 @@ class DBStorage:
         """ method to retrieve one object"""
         for key in classes:
             if cls is classes[key] or cls is key:
-                return __session.query(classes[key]).
-                filter(classes[key].id == id).all()
+                return __session.query(classes[key]).filter
+                (classes[key].id == id).all()
         return None
 
     def count(self, cls=None):
