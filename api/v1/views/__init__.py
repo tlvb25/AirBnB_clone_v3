f9e81b5743a42f867daaf99420387ca23f774df7
T <test@mail.com>
2019-09-11 16:13:22 -0700

    code changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ init file for views folder"""
 from flask import Blueprint
-from api.v1.views.index import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/vi")
+
+from api.v1.views.index import *
+
