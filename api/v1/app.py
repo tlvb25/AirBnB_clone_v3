cd1ebabec9a449eb0bc536565f53fb81f4582e7b
Amber <abmcbride5@gmail.com>
2019-09-11 06:33:59 +0000

    updating app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,12 @@
 """ app for task 4"""
 from flask import Flask, Blueprint
 from models import storage
-from api.v1.views import app_view
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
 
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
