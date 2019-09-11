9e86db30c0b0b22acf407174837be1ba677c1661
T <test@mail.com>
2019-09-11 16:19:48 -0700

    code changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,3 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix="/api/vi")
 
 from api.v1.views.index import *
-
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,8 @@
 from api.v1.views import app_views 
 from flask import jsonify
 
-if __name__ == "__main__":
-    @app_views.route('/status')
-    def j_string():
-        """ returns the json string status ok"""
-        return jsonify({"status": "OK"})
+
+@app_views.route('/status')
+def j_string():
+    """ returns the json string status ok"""
+    return jsonify({"status": "OK"})
