e744e2bcec6f874005f721731efb2cc21de4cac7
T <test@mail.com>
2019-09-11 18:45:20 -0700

    code for task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 app.register_blueprint(app_views)
@@ -16,6 +18,15 @@ app.register_blueprint(app_views)
 def tear_down(self):
     """teardown flask app"""
     storage.close()
+
+
+@app.errorhandler(404)
+@cross_origin()
+def error_handler():
+    """error handler"""
+    error = {"error": "Not found"}
+    return jsonify(error)
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST") or '0.0.0.0'
