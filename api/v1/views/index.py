9b2827f008d1acf6bd612d6eb228adac2afb53da
T <test@mail.com>
2019-09-11 17:52:19 -0700

    code changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,15 @@ from flask import jsonify
 def j_status():
     """ returns the json string status ok"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats')
+def j_stats():
+    stats_dict = {
+        "amenities": storage.count("Amenity"), 
+        "cities": storage.count("Cities"), 
+        "places": storage.count("Places"), 
+        "reviews": storage.count("Reviews"), 
+        "states": storage.count("States"), 
+        "users": storage.count("Users")
+    }
+    return jsonify(stats_dict)
