470ef2d7414dcaa8dcdd48bd15b3b2aed25c9272
T <test@mail.com>
2019-09-11 18:14:38 -0700

    code change

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,13 +10,14 @@ def j_status():
     """ returns the json string status ok"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats')
-def j_stats():
-    """ returns the json string stats ok"""
-    stats_dict = {"amenities": storage.count("Amenity"),
-                  "cities": storage.count("City"),
-                  "places": storage.count("Place"),
-                  "reviews": storage.count("Review"),
-                  "states": storage.count("State"),
-                  "users": storage.count("User")}
-    return jsonify(stats_dict)
+def stats():
+    """Stats of api"""
+    j_stats = {"amenities": storage.count("Amenity"),
+         "cities": storage.count("City"),
+         "places": storage.count("Place"),
+         "reviews": storage.count("Review"),
+         "states": storage.count("State"),
+         "users": storage.count("User")}
+    return jsonify(j_stats)
