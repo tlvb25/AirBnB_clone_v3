4bf063f0a002880ef140ef5ee90efcd827247e7d
T <test@mail.com>
2019-09-11 17:16:51 -0700

    code chg

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+
 
 app = Flask(__name__)
 
@@ -17,6 +17,6 @@ def close():
     storage.close()
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST") or '0.0.0.0'
-    port = getenv("HBNB_API_PORT") or 5000
+    host = '0.0.0.0'
+    port = 5000
     app.run(host=host, port=port, threaded=True)
