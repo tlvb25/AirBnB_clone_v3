a9e48bf0100c98d6aa318a5e9c29e26df62577df
T <test@mail.com>
2019-09-11 19:03:04 -0700

    code for task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ app for task 4"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -9,8 +9,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-
 app.register_blueprint(app_views)
 
 
@@ -22,10 +20,8 @@ def tear_down(self):
 
 @app.errorhandler(404)
 @cross_origin()
-def error_handler():
-    """error handler"""
-    error = {"error": "Not found"}
-    return jsonify(error)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
