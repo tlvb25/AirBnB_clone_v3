1094c5e46a3c5debcce05f27528389a28d80df24
T <test@mail.com>
2019-09-11 17:38:02 -0700

    code change

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
@@ -17,6 +18,6 @@ def tear_down(self):
     storage.close()
 
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    port = 5000
+    host = getenv("HBNB_API_HOST") or '0.0.0.0'
+    port = getenv("HBNB_API_PORT") or 5000
     app.run(host=host, port=port, threaded=True)
