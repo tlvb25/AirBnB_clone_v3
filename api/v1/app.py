262c5595fb49922cedc94962cae87450bb5a21f0
T <test@mail.com>
2019-09-13 10:11:53 -0700

    code chg

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={"origins": "0.0.0.0"})
 app.register_blueprint(app_views)
 
 
