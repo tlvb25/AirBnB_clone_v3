8e615d27959027ca9ae443380f5c9a13d49b0bfb
Amber <abmcbride5@gmail.com>
2019-09-13 06:29:22 +0000

    updating appy with cors instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_views)
 
 
