68802d4a009cb52d16a68ff303e6f976300221d5
Amber <abmcbride5@gmail.com>
2019-09-13 06:31:19 +0000

    changing app.py back

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(app_views)
 
 
